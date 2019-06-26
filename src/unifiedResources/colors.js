d6560687dd15801f758c668dabcd0077caf051f6
مولود آیت <m.ayat@aiegroup.local>
2019-06-26 12:40:04 +0430

    add color

---
+++ src/unifiedResources/colors.js
@@ -0,0 +1,15 @@
+const colors = {
+    // purpose
+    get button() { return this.redOrenge },
+    get background() { return this.extraLightGray },
+    get card() { return this.white },
+
+    // pallete
+    redOrenge: '#FE2E2E',
+    white: '#ffff',
+    extraLightGray: '#fcfcfe'
+
+
+
+}
+export default colors;
